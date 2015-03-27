113fc069fc4c88b88cb8b4f8efbaa7cb9b5f8dbb
Benjamin Bean <benjamin.bean@comcast.net>
2015-03-26 18:45:57 -0600

    Comments!

--- equalizer_connect_universal/equalizer_connect_universal.Shared/Track.cs
+++ equalizer_connect_universal/equalizer_connect_universal.Shared/Track.cs
@@ -6,39 +6,88 @@ using System.Threading.Tasks;
 
 namespace equalizer_connect_universal
 {
+    /// <summary>
+    /// A representation of a track being played from the server.
+    /// </summary>
     public class Track
     {
+        #region fields/properties
+
+        /// <summary>
+        /// Artist's name
+        /// </summary>
         private string artist;
+        /// <summary>
+        /// Artist's name.
+        /// Triggers <see cref="ChangedEvent"/>
+        /// </summary>
         public string Artist
         {
             get { return artist; }
             set
             {
-                artist = value;
-                ValueChanged(new TrackChangedEventArgs(
-                    "artist", artist));
+                if (artist != value)
+                {
+                    artist = value;
+                    ValueChanged(new TrackChangedEventArgs(
+                        "artist", artist));
+                }
             }
         }
+
+        /// <summary>
+        /// Track's title.
+        /// </summary>
         private string title;
+        /// <summary>
+        /// Track's title.
+        /// Triggers <see cref="ChangedEvent"/>
+        /// </summary>
         public string Title
         {
             get { return title; }
             set
             {
-                title = value;
-                ValueChanged(new TrackChangedEventArgs(
-                    "title", title));
+                if (title != value)
+                {
+                    title = value;
+                    ValueChanged(new TrackChangedEventArgs(
+                        "title", title));
+                }
             }
         }
 
+        #endregion
+
+        #region event handlers
+
+        /// <summary>
+        /// Triggered whenever either the artist or title are changed.
+        /// </summary>
         public EventHandler ChangedEvent;
 
+        #endregion
+
+        #region public methods
+
+        /// <summary>
+        /// Initializer a new instance of this class.
+        /// </summary>
         public Track()
         {
             artist = "";
             title = "";
         }
 
+        #endregion
+
+        #region private methods
+
+        /// <summary>
+        /// Triggers <see cref="ChangedEvent"/> as long
+        /// as it isn't null.
+        /// </summary>
+        /// <param name="args">The args to pass to the handlers.</param>
         private void ValueChanged(TrackChangedEventArgs args)
         {
             if (ChangedEvent != null)
@@ -47,14 +96,22 @@ namespace equalizer_connect_universal
             }
         }
 
-        public class TrackChangedEventArgs : EventArgs {
+        #endregion
+
+        #region classes
+
+        public class TrackChangedEventArgs : EventArgs
+        {
             public string property;
             public string newValue;
 
-            public TrackChangedEventArgs(string property, string newValue) {
+            public TrackChangedEventArgs(string property, string newValue)
+            {
                 this.property = property;
                 this.newValue = newValue;
             }
         }
+
+        #endregion
     }
 }
